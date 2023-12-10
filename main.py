be8fe60bde2e3b984bd480f33ead0af669881c65
almnaf <87938175+almnaf@users.noreply.github.com>
2023-12-10 18:08:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,48 +2,19 @@ import os
 from sudoku1 import Sudoku
 from dfs_algorithm import DFS
 
-
-def read_data(filename):
-  """
-    Reads the txt file and returns a list which containts the data of file 
-
-    Parameters
-    ----------
-    filename : str
-        The name of the file that user wants to read
-
-    
-    Raises
-    ------
-    FileNotFoundError
-      If the filename does not exist
-
-    Returns
-    -------
-    list
-        a list of lists which contains the data of the file
-  """
+#Membaca file '.txt' dan mengembalikan list sebagai data dari file input
+def read_data(filename): 
+  data = []
+  
   try:
-    data = []
-    abs_path = os.path.abspath(f"puzzles/{filename}")
-    with open(abs_path, "r", encoding="utf-8") as f:
-      contents = f.readlines()
-    for row in contents:
-      new_row = [int(number.strip()) for number in row if number != '\n' and number != ' ']
-      data.append(new_row)
-    return data
+      with open(f"puzzles/{filename}", "r", encoding="utf-8") as f:
+          data = [[int(number) for number in line.strip().split() if number] for line in f.readlines()]
   except FileNotFoundError:
-     return None
+      return None
+  return data
 
-
-
+#Panggil fungsi read_data dan buat objek sudoku baru. Kemudian jalankan algoritma DFS dan cetak sudoku yang sudah terselesaikan
 def main():
-  """
-    Call the read_data function and then creates a new sudoku object.
-    Finally creates an instance of DFS class, execute the algorithm
-    and prints the resutls of the algorithm 
-  """
-
   filename = input("Please enter the number of the text file: ")
   filename += ".txt"
   data = read_data(filename)
@@ -56,8 +27,5 @@ def main():
   else:
     print("This file does not exit. Please enter another file name")
 
-
 if __name__ == '__main__':
   main()
-
-
